0dd68c992ed987aad94da304763fbcf76f36dfb2
Al Khalil Alwaini <don.dndk@gmail.com>
2018-01-24 20:39:16 -0500

    Create Escrow.sol

---
+++ Escrow.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.19;
+
+contract Escrow{
+    
+    enum State {AWAITING_PAYMENT, AWAITING_DELIVERY, COMPLETE}
+    
+    State public currentState;
+    
+    address public buyer;
+    address public seller;
+    
+    modifier buyerOnly(){
+        require(msg.sender == buyer);
+        _;
+    }
+    
+    modifier inState(State expectedState){
+        require(currentState == expectedState);
+        _;
+    }
+    
+    function Escrow(address _buyer, address _seller){
+        buyer =_buyer;
+        seller = _seller;
+    }
+    
+    function confirmPayment() buyerOnly inState(State.AWAITING_PAYMENT) payable{
+        currentState = State.AWAITING_DELIVERY;
+    }
+    
+    function confirmDelivery() buyerOnly inState(State.AWAITING_DELIVERY) {
+        seller.send(this.balance);
+        currentState = State.COMPLETE;
+    }
+}
